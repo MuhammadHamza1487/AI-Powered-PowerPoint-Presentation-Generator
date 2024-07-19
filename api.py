82e63d280c4f07a684ba7956e9990b7cdd081add
Muhammad Hamza <muhammadhamza1487@gmail.com>
2024-07-19 17:15:31 +0500

    api.py

--- api.py
+++ api.py
@@ -1,1 +1,11 @@
 open_api_key = "Your Gemini API key here..."
+
+# How to get the API key?
+
+# 1. Proceed to the link "https://aistudio.google.com/app/" (Register yourself if you had'nt already)
+# 2. Accept all terms and conditions
+# 3. Click "GET API KEY" at the top left.
+# 4. Now click on "CREATE API KEY"
+# 5. Click "Understood"
+# 6. Click "Create API key in new project"
+# 7. Copy the API key and paste above.
